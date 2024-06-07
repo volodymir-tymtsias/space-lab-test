bd9823bb8ded07fc30c0d87a512a849aa95aab73
Volodymir Tymtsias <vovanuch2007@gmail.com>
2024-06-07 19:07:15 +0300

    added swiper in collection section and new-arrivals section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,53 @@
-'use strict';
+
+const collectionSwiper = new Swiper('.collection__wrapper', {
+  slidesPerView: 1,
+  autoHeight: true,
+  spaceBetween: 50,
+  
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 2,
+    },
+    1920: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const newArrivalsSwiper = new Swiper('.new-arrivals__content', {
+  slidesPerView: 1,
+  autoHeight: true,
+  spaceBetween: 23,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 4,
+    },
+  },
+});
